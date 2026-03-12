c7d26a4a7d772ef29e1feff8efaaf40289f6a1f9
murilobonoww <murilobonow07@gmail.com>
2026-03-12 15:15:59 -0300

    create tailwind.config.js

---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class", // ativa dark mode via classe
+  theme: {
+    extend: {
+      colors: {
+        primary: "#25f46a",
+        "background-dark": "#000000",
+      },
+      fontFamily: {
+        display: ["Space Grotesk", "sans-serif"], // sempre coloque fallback
+      },
+    },
+  },
+  plugins: [],
+};
